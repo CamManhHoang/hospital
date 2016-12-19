27a1e2e978c350d279219e279bdf312ee2fa3a98
Minh Hieu <minhhieu@kitfe.com>
2016-12-19 23:00:29 +0700

    session

--- app/check_login.php
+++ app/check_login.php
@@ -1,13 +1,12 @@
 <?php
 
 session_start();
-
 function check_login(){
-
-        $check = array(
-            "check_user"=>$_SESSION["session_user"],
-            "check_pass"=>$_SESSION["session_password"],
-            "check_session"=> $_SESSION["session_user"],
-        );
-        return $check;
+	$check = array(
+	    "check_user"=>$_SESSION["session_user"],
+	    "check_pass"=>$_SESSION["session_password"],
+	    "check_session"=> $_SESSION["session_user"],
+	);
+	return $check;
 }
+
